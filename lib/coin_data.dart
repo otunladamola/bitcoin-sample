855cad754eeae07401448cde531a04c9b68ce946
Damilola Alimi <alimidammy@gmail.com>
2023-03-08 14:01:14 +0100

    Fixed rate issue

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -37,31 +37,19 @@ const apiKey = '3C5B0AB6-F8DF-44E4-A8D3-AAA07C945F4F';
 
 class CoinData {
   //TODO 3: Update getCoinData to take the selectedCurrency as an input.
-  Future getCoinData(String selectedCurrency) async {
+  Future<double> getCoinData(String selectedCurrency) async {
     //TODO 4: Update the URL to use the selectedCurrency input.
     String requestURL = '$coinAPIURL/BTC?invert=false&apikey=$apiKey';
     http.Response response = await http.get(requestURL);
     if (response.statusCode == 200) {
       var decodedData = jsonDecode(response.body);
-
-      print(decodedData);
       double rate = 0.00;
-
       for (int i = 0; i < decodedData["rates"].length; i++) {
-        decodedData["rates"][i].forEach((key, value) {
-          final assetIds = value as List;
-          print('assetIds: $assetIds');
-          final selectedAssetId =
-              assetIds.where((element) => element == selectedCurrency).toList();
-          print('selectedAssetId: $selectedAssetId');
-
-          decodedData["rates"][i][key] = selectedAssetId.map((b) => print(b));
-        });
+        if (decodedData["rates"][i]["asset_id_quote"] == selectedCurrency) {
+          rate = decodedData["rates"][i]["rate"];
+        }
       }
-      print(rate);
-      double lastPrice = decodedData['rate'];
-
-      return lastPrice.toStringAsFixed(0);
+      return rate;
     } else {
       print(response.statusCode);
       throw 'Problem with the get request';
